d765154e637db8e9f7a2ddc036666fa3616b335d
antonshk2013 <anton.shkarupa@gmail.com>
2025-08-13 11:36:27 +0200

    feat: modify ListingSerializer, BookingSerializer

--- src/miet_angebot/serializers/bookings.py
+++ src/miet_angebot/serializers/bookings.py
@@ -4,5 +4,16 @@ from src.miet_angebot.models import Booking
 
 
 class BookingSerializer(serializers.ModelSerializer):
+    listing_title = serializers.SerializerMethodField()
     class Meta:
         model = Booking
+        fields = [
+            'id',
+            'listing_title',
+            'date_start',
+            'date_end',
+            'author',
+        ]
+
+    def get_listing_title(self, obj):
+        return obj.listing.title
--- src/miet_angebot/serializers/listings.py
+++ src/miet_angebot/serializers/listings.py
@@ -6,3 +6,14 @@ from src.miet_angebot.models import Listing
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
+        fields = [
+            'id',
+            'title',
+            'description',
+            'location',
+            'is_active',
+            'price_per_day',
+            'rooms_count',
+            'apartment_type',
+            'cancellation_policy',
+        ]
