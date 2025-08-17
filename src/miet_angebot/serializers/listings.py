e6ec9d5b3bdd1e8cc8423f87032b48f29d8ca29d
Antonshk2013 <antonshk2013@gmail.com>
2025-08-17 20:49:39 +0200

    feat: serializers add GuestListListingSerializer add GuestRetrieveListingSerializer add HostListingSerializer add HostRetrieveListingSerializer

--- src/miet_angebot/serializers/listings.py
+++ src/miet_angebot/serializers/listings.py
@@ -3,6 +3,59 @@ from rest_framework import serializers
 from src.miet_angebot.models import Listing
 
 
+class GuestListListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = [
+            'title',
+            'short_description',
+            'location',
+            'price_per_day',
+            'rooms_count',
+            'apartment_type',
+        ]
+
+class GuestRetrieveListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = [
+            'title',
+            'description',
+            'location',
+            'price_per_day',
+            'rooms_count',
+            'apartment_type',
+            'cancellation_policy',
+        ]
+
+class HostListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = [
+            'title',
+            'description',
+            'location',
+            'is_active',
+            'price_per_day',
+            'rooms_count',
+            'apartment_type',
+            'cancellation_policy',
+        ]
+
+class HostRetrieveListingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Listing
+        fields = [
+            'title',
+            'description',
+            'location',
+            'is_active',
+            'price_per_day',
+            'rooms_count',
+            'apartment_type',
+            'cancellation_policy',
+        ]
+
 class ListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Listing
