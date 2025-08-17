221a9f3f9bbf44db5fa5c900fe5661af7ec96e80
Antonshk2013 <antonshk2013@gmail.com>
2025-08-17 20:59:22 +0200

    feat: add get_queryset method to ListingViewSet

--- src/miet_angebot/views/listings.py
+++ src/miet_angebot/views/listings.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from src.miet_angebot.filters import ListingFilter
 from src.miet_angebot.models import Listing
 from src.miet_angebot.serializers import ListingSerializer
 
@@ -8,16 +9,23 @@ from src.miet_angebot.serializers import ListingSerializer
 class ListingViewSet(ModelViewSet):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
-    # permission_classes = [IsAuthenticated]
-    # http_method_names = ["get", "post", "put", "patch", "delete"]
-    #list, create, retrive + action Canseled + action Confirm
+    filterset_class = ListingFilter
+    search_fields = ["title", "description"]
+    ordering_fields = ["price_per_day", "created_at"]
+
+
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+    def get_queryset(self):
+        if self.request.user.groups.filter(name="host").exists():
+            return self.queryset.filter(author=self.request.user)
+        elif self.request.user.groups.filter(name="guest").exists():
+            return self.queryset.filter(is_active=True)
+        else:
+            return self.queryset.none()
 
-    #TODO
-    # def get_queryset(self):
-    #     ...
 
     #TODO
     # def get_serializer_class(self):
