af71777de1bf20a841cba6699a1a97013de91cde
Antonshk2013 <antonshk2013@gmail.com>
2025-08-24 20:06:07 +0200

    feat: set new ACCESS_TOKEN_LIFETIME 90 minut

--- core/settings.py
+++ core/settings.py
@@ -167,7 +167,7 @@ LOGGING = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=90),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
